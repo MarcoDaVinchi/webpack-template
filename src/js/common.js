1c72d424c08a0ef5ff9f0933dc05cac088cd6624
Marco <you@example.com>
2019-04-15 22:33:55 +0300

    js exercise

--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,28 @@
-const add = (a, b) => a + b;
-console.log(add(2, 8));
+const arr = [1, -2, 15, 2, 0, 8];
+const sortedA = arr.sort((a, b) => a - b);
+// console.log(sortedA);
+
+const obj = {
+  className: 'open menu',
+};
+
+function addClass(objToCheck, cls) {
+  let items = objToCheck.className ? objToCheck.className.split(' ') : [];
+  const res = objToCheck;
+  const store = {};
+  for (let i = 0; i <= items.length; i += 1) {
+    const key = items[i];
+    store[key] = true;
+  }
+  if (store[cls] !== true) {
+    items.push(cls);
+  }
+  items = items.join(' ');
+  res.className = items;
+  return res;
+}
+
+console.log(obj.className.split(' '));
+console.log(Object.keys(obj.className.split(' ')));
+
+addClass(obj, 'men');
