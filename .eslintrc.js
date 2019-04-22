ed370706d7e4d6ec744e7a15ecc680bd8bedaa42
MarcoDaVinchi <MagulaevMT@nevz.com>
2019-04-22 17:36:22 +0300

    react preset added to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-  extends: ['eslint:recommended', 'airbnb-base', 'plugin:vue/recommended'],
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'plugin:vue/recommended',
+    'plugin:react/recommended'
+  ],
   rules: {
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-  },
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }]
+  }
 };
